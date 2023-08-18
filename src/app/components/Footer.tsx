5171130ba7d6d666c076dbabe97e9ced61cfbcda
Gene Ni <geneisjuan@gmail.com>
2023-08-17 23:36:42 -0400

    nextui navbar

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,22 +1,42 @@
+'use client'
+
 import React from "react";
+import { Divider } from "@nextui-org/react";
 
 const Footer = () => {
-  const className = "mr-5 hover:text-light-primary duration-300";
+  const className = " mx-[20%] text-lg hover:text-light-primary duration-300";
 
   return (
-    <footer className="flex items-center w-[80%] justify-between py-[2%]">
-      <div className=" self-start flex md:flex-row flex-col">
+    // <footer className="flex items-center w-[80%] justify-between py-[2%]">
+    //   <div className=" self-start flex md:flex-row flex-col">
+    // <a className={className} href="">
+    //   Contact
+    // </a>
+    //     <Divider orientation="vertical" />
+    // <a className={className} href="">
+    //   Privacy
+    // </a>
+    // <a className={className} href="">
+    //   Terms
+    // </a>
+    //   </div>
+    //   <Divider orientation="vertical" className=" "/>
+    //   <h3>&copy; Copyright 2023 Discoreon</h3>
+    // </footer>
+    <footer className="">
+      <div className="flex h-5 items-center justify-center text-small my-[20%]">
         <a className={className} href="">
           Contact
         </a>
+        <Divider orientation="vertical" className=" border-dark-primary"/>
         <a className={className} href="">
           Privacy
         </a>
+        <Divider orientation="vertical" />
         <a className={className} href="">
           Terms
         </a>
       </div>
-      <h3>&copy; Copyright 2023 Discoreon</h3>
     </footer>
   );
 };
--- src/app/components/MainNav.tsx
+++ src/app/components/MainNav.tsx
@@ -1,123 +1,219 @@
 "use client";
 
-import Link from "next/link";
+// import Link from "next/link";
+// import Image from "next/image";
+// import React, { useState } from "react";
+// import Logo from "@/assets/discoreon_pokeball.png";
+// import { Poppins } from "next/font/google";
+// import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { useRouter } from "next/navigation";
+// import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+// import { SITE_URL } from "@/utils/globals";
+// import NavItem from "./NavItem";
+
+// import LoginButton from "@/app/components/LoginButton";
+// import { UserMetadata } from "@supabase/supabase-js";
+
+// // { user_metadata }: { user_metadata: UserMetadata | null }
+
+// const MainNav = () => {
+//   const [navbar, setNavbar] = useState("");
+//   const supabase = createClientComponentClient();
+//   const router = useRouter();
+//   const onToggleMenu = () => {
+//     navbar === "" ? setNavbar("top-[8%]") : setNavbar("");
+//   };
+
+//   const navOptions = [
+//     { label: "Get Started", href: "/start" },
+//     { label: "Invite", href: `/invite` },
+//     { label: "Premium", href: "/premium" },
+//     { label: "Showcase", href: "/showcase" },
+//   ];
+
+//   // console.log(user_metadata);
+
+//   // Image loader, required by Image/NextJS to load images from other sources.
+//   const loginHandler = async () => {
+//     // Creates a session object to retrieve the state of the session.
+
+//     const session = await supabase.auth.getSession();
+
+//     // If the session is null, that means the user is not logged in, therefore redirect them to discord oauth.
+//     if (!session.data.session) {
+//       const { data, error } = await supabase.auth.signInWithOAuth({
+//         provider: "discord",
+//         options: {
+//           redirectTo: `${SITE_URL}/api/auth`,
+//         },
+//       });
+//     } else {
+//       // If the session is authenticated, then take them directly to the home page.
+//       router.push("/home");
+//     }
+//   };
+
+//   const navItems = navOptions.map((item) => {
+//     return <NavItem key={item.label} item={item} />;
+//   });
+
+//   return (
+//     <nav className="flex justify-between items-center w-[100%] px-5 mx-auto md:sticky top-0 bg-white md:drop-shadow-2xl">
+//       <div>
+//         <Link href={"/"}>
+//           <Image
+//             className="w-16 cursor-pointer"
+//             src={Logo}
+//             alt="Discoreon Pokeball"
+//             width={70}
+//             height={70}
+//           />
+//         </Link>
+//       </div>
+
+//       <div
+//         className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5 ${navbar}`}
+//       >
+//         <ul className=" flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+//           {navItems}
+//           <li className="md:hidden block">
+//             <button
+//               onClick={loginHandler}
+//               className={`text-lg hover:text-light-primary duration-300`}
+//             >
+//               Login
+//             </button>
+//           </li>
+//         </ul>
+//       </div>
+
+//       <div className="flex items-center gap-6">
+//         <div className={`md:block hidden `}>
+//           {/* <div className={`md:${user_metadata ? "hidden" : "block"} hidden `}> */}
+//           <LoginButton />
+//         </div>
+//         {/* { navbar === "" ? <></> : <button> Login </button>} */}
+//         {/* <div className={`${
+//                 user_metadata ? "box" : "hidden"} `}>
+//           <Link href="/dashboard">
+//             <Image
+//               className={`border-2 rounded-full border-light-primary mb-[10%] "
+//               }`}
+//               // Since the foreign image source does not take a width property, Image/NextJS requires a unoptimized prop.
+//               unoptimized={imageLoader}
+//               width={50}
+//               height={50}
+//               src={user_metadata.picture}
+//               alt="Profile Picture"
+//             />
+//           </Link>
+//         </div> */}
+//         <button
+//           onClick={onToggleMenu}
+//           name="menu"
+//           className="text-3xl cursor-pointer md:hidden"
+//         >
+//           {navbar === "" ? <AiOutlineMenu /> : <AiOutlineClose />}
+//         </button>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default MainNav;
+import React from "react";
 import Image from "next/image";
-import React, { useState } from "react";
+// IoCloseOutline
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/react";
 import Logo from "@/assets/discoreon_pokeball.png";
-import { Poppins } from "next/font/google";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/navigation";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { SITE_URL } from "@/utils/globals";
-import NavItem from "./NavItem";
 
-import LoginButton from "@/app/components/LoginButton";
-import { UserMetadata } from "@supabase/supabase-js";
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-// { user_metadata }: { user_metadata: UserMetadata | null }
-
-const MainNav = () => {
-  const [navbar, setNavbar] = useState("");
-  const supabase = createClientComponentClient();
-  const router = useRouter();
-  const onToggleMenu = () => {
-    navbar === "" ? setNavbar("top-[8%]") : setNavbar("");
-  };
-
-  const navOptions = [
-    { label: "Get Started", href: "/start" },
-    { label: "Invite", href: `/invite` },
-    { label: "Premium", href: "/premium" },
-    { label: "Showcase", href: "/showcase" },
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
   ];
 
-  // console.log(user_metadata);
-
-  // Image loader, required by Image/NextJS to load images from other sources.
-  const loginHandler = async () => {
-    // Creates a session object to retrieve the state of the session.
-
-    const session = await supabase.auth.getSession();
-
-    // If the session is null, that means the user is not logged in, therefore redirect them to discord oauth.
-    if (!session.data.session) {
-      const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: "discord",
-        options: {
-          redirectTo: `${SITE_URL}/api/auth`,
-        },
-      });
-    } else {
-      // If the session is authenticated, then take them directly to the home page.
-      router.push("/home");
-    }
-  };
-
-  const navItems = navOptions.map((item) => {
-    return <NavItem key={item.label} item={item} />;
-  });
-
   return (
-    <nav className="flex justify-between items-center w-[100%] px-5 mx-auto md:sticky top-0 bg-white md:drop-shadow-2xl">
-      <div>
-        <Link href={"/"}>
-          <Image
-            className="w-16 cursor-pointer"
-            src={Logo}
-            alt="Discoreon Pokeball"
-            width={70}
-            height={70}
-          />
-        </Link>
-      </div>
-
-      <div
-        className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5 ${navbar}`}
-      >
-        <ul className=" flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-          {navItems}
-          <li className="md:hidden block">
-            <button
-              onClick={loginHandler}
-              className={`text-lg hover:text-light-primary duration-300`}
-            >
-              Login
-            </button>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex items-center gap-6">
-        <div className={`md:block hidden `}>
-          {/* <div className={`md:${user_metadata ? "hidden" : "block"} hidden `}> */}
-          <LoginButton />
-        </div>
-        {/* { navbar === "" ? <></> : <button> Login </button>} */}
-        {/* <div className={`${
-                user_metadata ? "box" : "hidden"} `}>
-          <Link href="/dashboard">
-            <Image
-              className={`border-2 rounded-full border-light-primary mb-[10%] "
-              }`}
-              // Since the foreign image source does not take a width property, Image/NextJS requires a unoptimized prop.
-              unoptimized={imageLoader}
-              width={50}
-              height={50}
-              src={user_metadata.picture}
-              alt="Profile Picture"
-            />
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarBrand>
+        <Image src={Logo} alt={"Bruh"} height={50} />
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
           </Link>
-        </div> */}
-        <button
-          onClick={onToggleMenu}
-          name="menu"
-          className="text-3xl cursor-pointer md:hidden"
-        >
-          {navbar === "" ? <AiOutlineMenu /> : <AiOutlineClose />}
-        </button>
-      </div>
-    </nav>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden decoration-dark-primary h-[10%]"
+          icon={isMenuOpen ? AiOutlineClose : AiOutlineMenu}
+        />
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color="foreground"
+              // color={
+              //   index === 2
+              //     ? "primary"
+              //     : index === menuItems.length - 1
+              //     ? "danger"
+              //     : "foreground"
+              // }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
-};
-
-export default MainNav;
+}
